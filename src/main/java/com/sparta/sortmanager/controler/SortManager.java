703867f3fc41195e31ffff58a98dad50537056fb
mandrei06 <marincasandrei06@gmail.com>
2022-04-20 17:55:50 +0100

    Updating my View (migrating the code from my controller to my view)

--- src/main/java/com/sparta/sortmanager/controler/SortManager.java
+++ src/main/java/com/sparta/sortmanager/controler/SortManager.java
@@ -1,40 +1,41 @@
 package com.sparta.sortmanager.controler;
 
-import com.sparta.sortmanager.model.BubbleSort;
-import com.sparta.sortmanager.model.MergeSort;
 import com.sparta.sortmanager.model.SortFactory;
 import com.sparta.sortmanager.model.SortMethod;
+import com.sparta.sortmanager.view.DisplayManager;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
 public class SortManager {
     public static void main(String[] args) {
-        final long startTime = System.currentTimeMillis();
-        //Input for algorithm choice
-        System.out.println("Please choose number:\n1-For bubble sort\n2-For merge sort");
-        Scanner scanner=new Scanner(System.in);
-        int algorithmChoice=scanner.nextInt();
 
+        //Input for algorithm choice
+        DisplayManager displayManager=new DisplayManager();
+        int algorithmChoice = displayManager.inputSortMethod();
 
         //Input for array length
-        System.out.println("Please enter an integer number for the length of the array");
-        int arrayLength=scanner.nextInt();
-        int[] numbersArray = new int[arrayLength];
+        int arrayLength=displayManager.inputArrayLength();
 
+        //Move this to MODEL
+        //Move this to MODEL
+        //Move this to MODEL
+        //Move this to MODEL
+        //Move this to MODEL
         //Creating a random integers array
+        int[] numbersArray = new int[arrayLength];//move this to MODEL
         for (int i = 0; i < arrayLength; i++) {
             numbersArray[i] = new Random().nextInt(2000)-1000;//between -1000 and +1000
         }
 
         //Printing the unsorted array
         System.out.println(Arrays.toString(numbersArray));
+        final long startTime = System.currentTimeMillis();
         SortMethod sortMethod= SortFactory.getSortMethod(algorithmChoice);
         System.out.println("You choose to use:");
         System.out.println(sortMethod.getClass().getSimpleName());//Add error handling for null
         final long endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime - startTime)+" milliseconds (1 ms = 0.001s)");
+        System.out.println("Total time for sorting: " + (endTime - startTime)+" milliseconds (1 ms = 0.001s)");
 
     }
 
--- src/main/java/com/sparta/sortmanager/view/DisplayManager.java
+++ src/main/java/com/sparta/sortmanager/view/DisplayManager.java
@@ -1,4 +1,16 @@
 package com.sparta.sortmanager.view;
 
+import java.util.Scanner;
+
 public class DisplayManager {
+    Scanner scanner=new Scanner(System.in);
+
+    public int inputSortMethod(){
+        System.out.println("Please choose number:\n1-For bubble sort\n2-For merge sort");
+        return scanner.nextInt();
+    }
+    public int inputArrayLength(){
+        System.out.println("Please enter an integer number for the length of the array");
+        return scanner.nextInt();
+    }
 }
